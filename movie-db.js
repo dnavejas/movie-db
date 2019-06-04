c0d1696480d1809206222e281326db0114854002
Danny Navejas <dannynavejas@gmail.com>
2019-06-04 17:53:43 -0500

    added new page to display search results. search results now display images

--- movie-db.js
+++ movie-db.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $("#search-submit").click(function (e) { 
+        e.preventDefault()
         let searchCriteria = $("#search-bar")[0].value;
         let result = searchCriteria.split(" ").join("%20");
         let link = `${"https://api.themoviedb.org/3/search/multi?api_key=9d11c1773b21ad7a4a5761cc1454361b&language=en-US&query="+result+"&page=1&include_adult=false"}`
@@ -17,14 +18,32 @@ $(document).ready(function () {
             let searchResults = response.results;
             $.each(searchResults, function(i, value){
                 let searchObjects = value;
-                $.each(searchObjects, function (i, value){
+                let img = searchObjects.poster_path;
+                // console.log(value);
+
+                // Create images
+                imageTag = document.createElement("img");
+                imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + img}`
+                $("#load-search-data").append(imageTag)
+
+                $.each(searchObjects, function(i, value){
                     let div = document.createElement("div");
-                    $("#load-search-data").append(div, value)
+                    div.innerHTML = value;
+                    $("#load-search-data").append(div);
                 })
-               
+
+
+                
+                // if(value = img){
+                //     imageTag = document.createElement("img");
+                //     imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + value}`
+                //     $("#load-search-data").append(imageTag)
+                // } else() => $.each(searchObjects, function (i, value){
+                //     console.log(value);
+                //     let div = document.createElement("div");
+                //     $("#load-search-data").append(div, value);
+                // })
             });
         });        
     });
-    // function captureSearch(){
-    // }
 });

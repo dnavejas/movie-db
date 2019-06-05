8fe5cda2a818b1cefd17afe1bc73be222311cc72
Danny Navejas <dannynavejas@gmail.com>
2019-06-04 20:08:16 -0500

    search results return in a new page.

--- movie-db.js
+++ movie-db.js
@@ -1,49 +1,90 @@
 $(document).ready(function () {
     $("#search-submit").click(function (e) { 
         e.preventDefault()
-        let searchCriteria = $("#search-bar")[0].value;
-        let result = searchCriteria.split(" ").join("%20");
-        let link = `${"https://api.themoviedb.org/3/search/multi?api_key=9d11c1773b21ad7a4a5761cc1454361b&language=en-US&query="+result+"&page=1&include_adult=false"}`
 
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": link,
-            "method": "GET",
-            "headers": {},
-            "data": "{}"
-        }
-          
-        $.ajax(settings).done(function (response) {
-            let searchResults = response.results;
-            $.each(searchResults, function(i, value){
-                let searchObjects = value;
-                let img = searchObjects.poster_path;
-                // console.log(value);
+        let searchCriteria = $("#search-bar")[0].value;
+
+        window.location.href = `search-results.html?query=${searchCriteria}`;
+    });
+    GetData();
+    loadCarousel();
+});
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+function GetData(){
+    let searchCriteria = getUrlParameter('query');
+    let link = `${"https://api.themoviedb.org/3/search/multi?api_key=9d11c1773b21ad7a4a5761cc1454361b&language=en-US&query="+searchCriteria+"&page=1&include_adult=false"}`
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": link,
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    }
+        
+    $.ajax(settings).done(function (response) {
+        let searchResults = response.results;
+        $.each(searchResults, function(i, value){
+            let searchObjects = value;
+            console.dir(searchObjects);
+            if (searchObjects.media_type === "movie"){
+                let movieImg = searchObjects.poster_path;
+                let title = searchObjects.original_title;
+                let rating = searchObjects.vote_average;
+                let overview = searchObjects.overview;
+                let div = document.createElement("div");
+                div.innerHTML = title + "<br>" + rating + "<br>" + overview;
+                let target = $("#load-search-data")
+
 
                 // Create images
-                imageTag = document.createElement("img");
-                imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + img}`
-                $("#load-search-data").append(imageTag)
-
-                $.each(searchObjects, function(i, value){
-                    let div = document.createElement("div");
-                    div.innerHTML = value;
-                    $("#load-search-data").append(div);
+                var imageTag = document.createElement("img");
+                imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + movieImg}`
+                target.append(imageTag);
+                target.append(div);
+            } else if (searchObjects.media_type === "person") {
+                let personImg = searchObjects.profile_path;
+                let name = searchObjects.name;
+                let movies = searchObjects.known_for;
+                let works = $.each(movies, function(i, value){
+                    
+                    let workName = value.original_title;
+                    return workName;
                 })
+                console.log(works); 
 
 
-                
-                // if(value = img){
-                //     imageTag = document.createElement("img");
-                //     imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + value}`
-                //     $("#load-search-data").append(imageTag)
-                // } else() => $.each(searchObjects, function (i, value){
-                //     console.log(value);
-                //     let div = document.createElement("div");
-                //     $("#load-search-data").append(div, value);
-                // })
-            });
-        });        
-    });
-});
+                let div = document.createElement("div");
+                div.innerHTML = "<h2>" + name + "</h2>" + "<br>" + "List of Movies/Shows:" + "<br>" + works;
+                let target = $("#load-search-data")
+                var imageTag = document.createElement("img");
+                imageTag.src = `${"https://image.tmdb.org/t/p/w500/" + personImg}`
+                target.append(imageTag);
+                target.append(div);
+
+            }
+        }) 
+    });        
+}   
+function loadCarousel(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/movie/upcoming?api_key=9d11c1773b21ad7a4a5761cc1454361b&language=en-US&page=1",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+        }
+        
+        $.ajax(settings).done(function (response) {
+        console.dir(response.results);
+        });
+    }
